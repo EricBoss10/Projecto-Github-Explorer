ac4676045b76e85ba3b3e00017153926767a4417
Eric Fernandes <ericf9333@gmail.com>
2020-11-15 23:26:55 -0100

    Frontend github Explorer

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositorios no Github</Title>;
+};
+
+export default Dashboard;
