91691dd384cb5fabccf69a65f479095807f1e991
Eric Fernandes <ericf9333@gmail.com>
2020-11-16 23:23:16 -0100

    Frontend github Explorer class7

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Switch , Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Respository from '../pages/Repository';
 
-const Routes: React.FC = () =>(
-    <Switch>
-        <Route path="/" exact component={Dashboard}/>
-        <Route path="/repository" component={Respository}/>
-    </Switch>
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Respository} />
+  </Switch>
 );
 
 export default Routes;
